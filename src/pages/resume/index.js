77e4875ca80e2b90e1d24a87bc7fb5278938a0c8
Dustin Schau <dustinschau@gmail.com>
2022-01-30 21:12:50 -0800

    feat: implement bulk of resume

--- src/pages/resume/index.js
+++ src/pages/resume/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { graphql } from 'gatsby'
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
-import Box from './box'
+import '@fontsource/raleway'
+import '@fontsource/hahmlet'
 
 import * as styles from './index.module.css';
+
+import Box from './box'
+import Experience from './experience'
 
 export default function Resume({ data }) {
   return (
@@ -14,7 +18,7 @@ export default function Resume({ data }) {
           <Box>Engineering &amp; Product Leader</Box>
           <h1 className={styles.title}>Hello! I'm Dustin Schau.</h1>
           <p className={styles.bioText}>
-            I'm a Product and Engineering leader who has experience in scaling Product delivery at hyper-growth startups. I care <strong>quite a lot</strong> about the details, and believe that the best performing Engineering teams blend the best parts of Product, Engineering, and Design.
+            I'm a Product and Engineering leader who has experience in scaling Product delivery at hyper-growth startups. I care <strong>quite a lot</strong> about the details, and believe that the best performing Engineering teams pull insights, practices, and expertise from Product, Engineering, and Design, without losing sight of what matters most: <span class={styles.under}>building a product that delights the customer</span>.
           </p>
         </div>
         <div className={[styles.cell, styles.image].join(' ')}>
@@ -24,6 +28,58 @@ export default function Resume({ data }) {
       <section className={[styles.grid].join(' ')}>
         <div className={[styles.cell, styles.experience].join(' ')}>
           <h2 className={styles.subTitle}>Experience</h2>
+          <Experience
+            jobTitle="VP of Engineering"
+            employer="Gatsby, Inc. • Fully Distributed"
+            dates={[
+              new Date('08/01/2021')
+            ]}
+            description="Leads the 30+ Engineering building the fastest web framework for the headless web"
+            items={[
+              'Launched Gatsby 4, our most fully featured release yet with game-changing features like DSG (deferred static generation) and SSR (server-side rendering)',
+              'Did another thing'
+            ]}
+          />
+          <Experience
+            jobTitle="VP of Product"
+            employer="Gatsby, Inc. • Fully Distributed"
+            dates={[
+              new Date('12/01/2019'),
+              new Date('07/31/2021')
+            ]}
+            description="Joined as the 11th employee at Gatsby as an engineer, and built and founded the Product team at Gatsby"
+            items={[
+              'Helped establish and implement product vision and helped build an open-core business model which increased revenue from $400K to $2.5M ARR',
+              'Established Product Council, a quarterly planning and collaborative process wherein top-down strategy and autonomy are blended to empower teams to deliver strategically valuable product offerings',
+              'Grew the Product team from 1 to 8 (including Design), and organized the teams into cross-functional squads composed of Product, Engineering, and Design'
+            ]}
+          />
+          <Experience
+            jobTitle="Principal Consultant"
+            employer="Object Partners • Omaha, Nebraska"
+            dates={[
+              new Date('05/01/2016'),
+              new Date('07/01/2018')
+            ]}
+            description="Serve as a technical consultant to assist teams, companies, and organizations by assisting and leading developmental efforts, improving in-house developer knowledge, and establishing best practices."
+            items={[
+              'Implented quarterly planning process and high-level key-product themes',
+              'Helped establish product vision and market to increase revenue from $400K to $2.5M ARR'
+            ]}
+          />
+          <Experience
+            jobTitle="Senior Application Developer"
+            employer="Union Pacific • Omaha, Nebraska"
+            dates={[
+              new Date('04/01/2015'),
+              new Date('05/01/2016')
+            ]}
+            description="I began my career at Union Pacific, honing my front-end developer expertise and knowledge by creating mobile/hybrid applications, creating an application generator, and building an internal open-source component library (a la Bootstrap) and associated tooling."
+            items={[
+              'Built a reusable, accessible component library with AngularJS used by all teams at Union Pacific',
+              'Built a next-generation application generator to speed up delivery to cut down startup time by 25%'
+            ]}
+          />
         </div>
         <div className={styles.details}>
           <Box>Contact Info</Box>
@@ -47,9 +103,16 @@ export default function Resume({ data }) {
             <li>Hyper-growth Startups</li>
             <li>Leading with Kindness</li>
           </ul>
+          {/* <Box>Side Projects</Box>
+          <ul>
+            <li>CSS in JS Playground</li>
+            <li>Algorithm Playground</li>
+            <li>CSS in JS Podcast with Chris Coyier</li>
+            <li>Leading with Kindness</li>
+          </ul> */}
         </div>
       </section>
-      <a href="/api/page-to-pdf?path=/resume/">Download PDF</a>
+      <a className={styles.downloadResume} href="/api/page-to-pdf?path=/resume/">Download PDF</a>
     </main>
   );
 }
